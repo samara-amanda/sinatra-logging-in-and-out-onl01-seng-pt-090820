569cd9a3fcd2a1e78bc5c9c6aee78545559f7e59
Samara Grant <samara.amanda01@gmail.com>
2020-12-01 18:16:54 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
   def self.current_user(session_hash)
-    User.find(session_hash["user_id"])
+    User.find(session_hash[:user_id])
   end
 
   def self.is_logged_in?(session_hash)
     binding.pry
-    !!session_hash["user_id"]
+    !!session_hash[:user_id]
   end
 
 end
